0a20c2918ce9906fae4ef4b9cfc20afa927a7d40
JP Addison <johnpaddison@gmail.com>
2020-06-12 16:40:37 -0700

    export comment details

--- packages/lesswrong/server/scripts/exportPostDetails.ts
+++ packages/lesswrong/server/scripts/exportPostDetails.ts
@@ -26,6 +26,7 @@ import fs from 'mz/fs';
 import Papa from 'papaparse';
 import path from 'path';
 import { Posts } from '../../lib/collections/posts';
+import { Comments } from '../../lib/collections/comments';
 import Users from '../../lib/collections/users/collection';
 import { siteUrlSetting } from '../../lib/instanceSettings';
 import { Vulcan } from '../vulcan-lib';
@@ -118,6 +119,89 @@ Vulcan.exportPostDetailsByMonth = ({month, outputDir, outputFile}) => {
   //eslint-disable-next-line no-console
   console.log(`Exporting all posts from ${lastMonth.format('MMM YYYY')}`)
   return Vulcan.exportPostDetails({
+    selector: {
+      createdAt: {
+        $gte: lastMonth.toDate(), // first of prev month
+        $lte: moment.utc(lastMonth).endOf('month').toDate()
+      }
+    },
+    outputFile,
+    outputDir
+  })
+}
+
+function getComments (selector) {
+  const defaultSelector = {
+    authorIsUnreviewed: false,
+    spam: {$ne: true},
+    delted: {$ne: true},
+  }
+
+  const fields = {
+    id: 1,
+    userId: 1,
+    postId: 1,
+    postedAt: 1,
+    createdAt: 1,
+    baseScore: 1,
+    deleted: 1,
+    answer: 1,
+    spam: 1,
+    authorIsUnreviewed: 1,
+  }
+
+  const finalSelector = Object.assign({}, defaultSelector, selector || {})
+  console.log('finalSelector', finalSelector)
+
+  return Comments
+    .find(finalSelector, {fields, sort: { createdAt: 1 }})
+}
+
+Vulcan.exportCommentDetails = wrapVulcanAsyncScript(
+  'exportCommentDetails',
+  async ({selector, outputDir, outputFile = 'comment_details.csv'}) => {
+    if (!outputDir) throw new Error('you must specify an output directory (hint: {outputDir})')
+    const documents = getComments(selector)
+    let c = 0
+    const count = documents.count()
+    console.log('count', count)
+    const rows: Array<any> = []
+    for (let comment of documents.fetch()) {
+      // SD: this makes things horribly slow, but no idea how to do a more efficient join query in Mongo
+      const user = Users.findOne(comment.userId, { fields: { displayName: 1, email: 1 }})
+      const post = Posts.findOne(comment.postId, { fields: { slug: 1, title: 1 }})
+      if (!user) throw Error(`Can't find user for comment: ${comment._id}`)
+      const baseUrl = siteUrlSetting.get()
+      const row = {
+        display_name: user.displayName,
+        email: user.email,
+        post_title: post?.title,
+        id: comment._id,
+        user_id: comment.userId,
+        karma: comment.baseScore,
+        posted_at: comment.postedAt,
+        created_at: comment.createdAt,
+        url: `${baseUrl}/posts/${comment.postId}/${post?.slug}?commentId=${comment?._id}`
+      }
+      rows.push(row)
+      c++
+      //eslint-disable-next-line no-console
+      if (c % 20 === 0) console.log(`Comment Details: Processed ${c}/${count} comments (${Math.round(c / count * 100)}%)`)
+    }
+    const csvFile = Papa.unparse(rows)
+    const filePath = path.join(outputDir,`${path.basename(outputFile)}.csv`)
+    await fs.writeFile(filePath, csvFile)
+    //eslint-disable-next-line no-console
+    console.log(`Wrote details for ${rows.length} comments to ${filePath}`)
+  }
+)
+
+Vulcan.exportCommentDetailsByMonth = ({month, outputDir, outputFile}) => {
+  const lastMonth = moment.utc(month, 'YYYY-MM').startOf('month')
+  outputFile = outputFile || `comment_details_${lastMonth.format('YYYY-MM')}`
+  //eslint-disable-next-line no-console
+  console.log(`Exporting all comments from ${lastMonth.format('MMM YYYY')}`)
+  return Vulcan.exportCommentDetails({
     selector: {
       createdAt: {
         $gte: lastMonth.toDate(), // first of prev month
