798029d8188e5dc503fb6d5236d8461f356e1d59
JP Addison <johnpaddison@gmail.com>
2020-08-18 17:57:39 +0100

    newMutation in migration

--- packages/lesswrong/server/migrations/2020-08-09-metaToCommunity.ts
+++ packages/lesswrong/server/migrations/2020-08-09-metaToCommunity.ts
@@ -4,10 +4,7 @@ import Users from '../../lib/collections/users/collection'
 import Tags from '../../lib/collections/tags/collection'
 import Posts from '../../lib/collections/posts/collection';
 import TagRels from '../../lib/collections/tagRels/collection';
-import * as _ from 'underscore';
-import moment from 'moment';
-import { updatePostDenormalizedTags } from '../tagging/tagCallbacks';
-import { recomputeDenormalizedValues } from '../scripts/recomputeDenormalized';
+import { newMutation } from '../vulcan-lib';
 
 // TODO; Doc
 // Previously, the personalBlog weight was being hackily used to refer
@@ -67,9 +64,6 @@ registerMigration({
   }
 })
 
-// TODO; make sure there's nothing too funny about number of votes or similar
-// TODO; If I'm giving up on performance, I could use newMutation and
-// performVoteServer as in tagsGraphQL.ts
 registerMigration({
   name: 'metaToCommunityPosts',
   dateWritten: '2020-08-12',
@@ -84,49 +78,28 @@ registerMigration({
       callback: async (posts: Array<DbPost>) => {
         // eslint-disable-next-line no-console
         console.log("Migrating post batch");
-        // If I were to write this again, I'd call the newMutation method, which would get me a bunch of callbacks firing automatically,
-        const tagRelInserts = posts.map(post => ({
-          insertOne: {
-            tagId : communityTagId,
-            postId : post._id,
-            userId : post.reviewedByUserId,
-            baseScore : 2,
-            score : 2,
-            afBaseScore : 2,
-            voteCount : 1,
-            deleted : false,
-            schemaVersion : 1,
-            inactive : false,
-            createdAt : moment().toDate(),
-          }
-        }))
-        if (tagRelInserts.length) {
-          const result = await TagRels.rawCollection().bulkWrite(tagRelInserts, { ordered: false });
-          // eslint-disable-next-line no-console
-          console.log(`updated ${result.result.nModified} posts`)
-        }
         
-        // TagRels exist, but the callbacks did not fire, which means posts have
-        // not updated to store the tag relevance on their object
-        for (let post of posts){
-          // This is two database calls for every meta post. I believe that this
-          // is only annoyingly and not excruciatingly slow. It seems like I'll
-          // need to open up this function and write it with an interesting join
-          // if I want to avoid it.
-          await updatePostDenormalizedTags(post._id)
+        for (let post of posts) {
+          // Oh man, I refactored this migration to use this method, and it
+          // changed my life. 10/10 would use again in future migrations.
+          // bulkwrite is faster, but callbacks are often important
+          await newMutation({
+            collection: TagRels,
+            document: {
+              tagId: communityTagId,
+              postId: post._id,
+              userId: post.reviewedByUserId,
+              baseScore: 2,
+            },
+            // Validation requires us to have a context object, which has
+            // things like currentUser that aren't applicable.
+            validate: false,
+          })
         }
-        
-        // Again because of TODO;
-        await recomputeDenormalizedValues({collectionName: "Tags", fieldName: "postCount"})
       }
-    });
-    
-    // Because of the bulkWrite nature of our insert, we ended up with ObjectIDs
-    // instead of strings for the IDs. Our code doesn't like those, so replace
-    // them.
-    await replaceObjectIdsWithStrings(TagRels)
+    })
   }
-});
+})
 
 registerMigration({
   name: 'moveMetaToFrontpage',
@@ -155,39 +128,6 @@ registerMigration({
           console.log(`updated ${result.result.nModified} posts`)
         }
       }
-    });
+    })
   }
-});
-
-// Copy pasta from a migration that hasn't been used in a year
-// See: `2019-02-04-replaceObjectIdsInEditableFields.ts`
-async function replaceObjectIdsWithStrings<T extends DbObject> (collection: CollectionBase<T>) {
-  await migrateDocuments({
-    description: `Replace object ids with strings in ${collection.collectionName}`,
-    collection,
-    batchSize: 1000,
-    unmigratedDocumentQuery: {
-      _id: {$type: "objectId"}
-    },
-    migrate: async (documents: Array<T>) => {
-      const updates = documents.map(doc => {
-        return {
-          updateOne: {
-            filter: {_id: doc._id.valueOf()},
-            update: {
-              ...doc,
-              _id: doc._id.valueOf(),
-            },
-            upsert: true
-          }
-        }
-      })
-      await collection.rawCollection().bulkWrite(
-        updates,
-        { ordered: false }
-      )
-      const _ids = _.pluck(documents, '_id')
-      await collection.remove({_id: {$in: _ids}})
-    }
-  })
-}
+})
