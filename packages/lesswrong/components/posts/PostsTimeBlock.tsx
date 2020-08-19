16fafd262d79534a5e7dacd7e96afc8ffbbf8037
JP Addison <johnpaddison@gmail.com>
2020-08-19 18:08:41 +0100

    better dealing with potentially null posts

--- packages/lesswrong/components/posts/PostsTimeBlock.tsx
+++ packages/lesswrong/components/posts/PostsTimeBlock.tsx
@@ -131,7 +131,7 @@ class PostsTimeBlock extends Component<PostsTimeBlockProps,PostsTimeBlockState> 
 
     const postGroups = postTypes.map(type => ({
       ...type,
-      posts: posts?.filter(type.postIsType)
+      posts: posts?.filter(type.postIsType) || []
     }))
 
     return (
@@ -170,13 +170,13 @@ class PostsTimeBlock extends Component<PostsTimeBlockProps,PostsTimeBlockState> 
           </div> }
 
           {postGroups.map(({name, posts, label}) => {
-            if (posts!.length > 0) return <div key={name}>
+            if (posts?.length > 0) return <div key={name}>
               <div
                 className={name === 'frontpage' ? classes.frontpageSubtitle : classes.otherSubtitle}
               >
                 <ContentType type={name} label={label} />
               </div>
-              {posts!.map((post, i) =>
+              {posts.map((post, i) =>
                 <PostsItem2 key={post._id} post={post} index={i} dense />
               )}
             </div>
